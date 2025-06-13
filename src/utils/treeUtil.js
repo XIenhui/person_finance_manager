b2d01eda4a7868d8d84aa0ae0656c9c9715f7c3b
XIenhui <1145229001@qq.com>
2025-06-13 16:26:42 +0800

    better tree build

--- src/utils/treeUtil.js
+++ src/utils/treeUtil.js
@@ -1,17 +1,42 @@
+import {error} from "#utils/response.js";
+
 /**
  * 将扁平数组转换为树形结构
  * @param {Array} items 分类项数组
- * @param {Number|null} parentId 父ID
+ * @param id_name ID属性名
+ * @param {string} parent_name 父ID属性名
+ * @param {Number|null} rootParentId 根ID
  * @returns {Array} 树形结构
  */
-export function buildTree(items, parentId = null) {
-    return items
-        .filter(item => item.parent_id === parentId)
-        .map(item => ({
-            ...item,
-            children: buildTree(items, item.id)
-        }));
+export function buildTree(items, id_name = 'id', parent_name = 'parent_id', rootParentId = null) {
+    const itemMap = new Map();
+    const tree = [];
+    const filterItems = items.filter(item => !!item[id_name])
+    for (const item of filterItems) {
+        itemMap.set(item[id_name], { ...item, children: [] });
+    }
+    try {
+        for (const item of filterItems) {
+            const id = item[id_name];
+            const parentId = item[parent_name];
+            const treeItem = itemMap.get(id);
+
+            if (parentId === rootParentId || parentId == null) tree.push(treeItem);
+            else {
+                const parentItem = itemMap.get(parentId);
+                if (parentItem) {
+                    parentItem.children.push(treeItem);
+                } else {
+
+                }
+            }
+        }
+    } catch (error) {
+        throw error
+    }
+    return tree;
 }
+
 
 /**
  * 生成分类完整路径名称
